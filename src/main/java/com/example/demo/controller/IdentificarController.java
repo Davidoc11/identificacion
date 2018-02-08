3c59ed3708b33e0fab82eaef8cf22248db2f13fb
Oscar <Oscar@192.168.10.196>
2018-02-07 19:35:41 -0600

    [UPDATE]- new mofification in IdentificarController.java and comments of this part

--- src/main/java/com/example/demo/controller/IdentificarController.java
+++ src/main/java/com/example/demo/controller/IdentificarController.java
@@ -52,4 +52,6 @@ public class IdentificarController {
     void handleBadRequests(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value());
     }*/
+    
+    //ALV me vale verga prro, y jalate hDPM
 }
